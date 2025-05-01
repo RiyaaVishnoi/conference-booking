5a530bb478ab4b3161caafd97be8e22b7866fdd1
Riya V <riavishnoi6@yahoo.com>
2025-05-01 23:14:19 +1200

    Add user registration, login, and My Bookings page

--- rooms/urls.py
+++ rooms/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.room_list, name='room_list'),
     path('book/', views.book_room, name='book_room'),
+    path('my-bookings/', views.my_bookings, name='my_bookings'),
 
 ]
--- rooms/views.py
+++ rooms/views.py
@@ -3,6 +3,9 @@ from .models import Room
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from .forms import BookingForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+from .models import Booking
 
 # get all rooms from db and pass them to room list
 def room_list(request):
@@ -23,3 +26,20 @@ def book_room(request):
         form = BookingForm()
 
     return render(request, 'rooms/book_room.html', {'form': form})
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # log them in after signup
+            return redirect('room_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'rooms/register.html', {'form': form})
+
+
+@login_required
+def my_bookings(request):
+    bookings = Booking.objects.filter(user=request.user)
+    return render(request, 'rooms/my_bookings.html', {'bookings': bookings})
