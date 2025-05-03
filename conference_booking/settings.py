93c5568e055ebf2cd1b96d2b623c3760222d4478
Riya V <riavishnoi6@yahoo.com>
2025-05-03 18:41:55 +1200

    Fix static files for admin UI

--- conference_booking/settings.py
+++ conference_booking/settings.py
@@ -133,7 +133,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATICFILES_DIRS = [
+  #  os.path.join(BASE_DIR, 'static')
+#]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -142,5 +146,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 if os.environ.get('VERCEL'):
     ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
